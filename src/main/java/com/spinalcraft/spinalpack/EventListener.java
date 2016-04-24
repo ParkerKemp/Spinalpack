d9503ada9fdca0f3c0478b1263e50cf690f67aee
ParkerKemp <charles.kemp702@topper.wku.edu>
2016-04-24 18:12:59 -0500

    Add alternate potion effect for chorus fruit

--- src/main/java/com/spinalcraft/spinalpack/EventListener.java
+++ src/main/java/com/spinalcraft/spinalpack/EventListener.java
@@ -1,17 +1,23 @@
 package com.spinalcraft.spinalpack;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class EventListener implements Listener {
 
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerTeleport(PlayerTeleportEvent event){
 		TeleportCause cause = event.getCause();
-		if(cause == TeleportCause.CHORUS_FRUIT)
-			event.setCancelled(true);
+		if(cause != TeleportCause.CHORUS_FRUIT)
+			return;
+		event.setCancelled(true);
+		Player player = event.getPlayer();
+		player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 10, 1));
 	}
 }
